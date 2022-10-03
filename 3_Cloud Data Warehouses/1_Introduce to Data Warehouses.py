1e94e38cf55b09d3729e008eb411c0008a3c543d
VietCaheo <vietdaongoc@gmail.com>
2022-10-04 06:35:46 +0700

    up DataWH

--- 3_Cloud Data Warehouses/1_Introduce to Data Warehouses.py
+++ 3_Cloud Data Warehouses/1_Introduce to Data Warehouses.py
@@ -1,1 +1,10 @@
-"""  """
+""" 
+in Lession 1:
+ -> What is a Data Warehouse (in a business perspective)
+ -> What is a Data Warehouse (in a technical perspective)
+ -> Dimension modeling
+ -> DWH architecture
+ -> OLAP Cubes
+ -> DWH storgae technology
+ 
+ """
