aed13d4dbf3b976daaa17a1f009eb61cfb8b8bca
VietCaheo <vietdaongoc@gmail.com>
2023-01-02 09:37:29 +0700

    up success submission Prj5

---
+++ Proj5_Data Pipeline/submission/home/airflow/dags/udac_example_dag.py
@@ -0,0 +1,145 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator, PostgresOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+
+# The DAG does not have dependencies on past runs
+# On failure, the task are retried 3 times
+# Retries happen every 5 minutes
+# Catchup is turned off
+# Do not email on retry
+
+default_args = {
+    'depends_on_past': False,
+    'owner': 'udacity',
+    'start_date': datetime.now(),
+    'email': ['vietdaongoc@gmail.com'],
+    'email_on_retry': False,
+    'email_on_failure': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'dicts_checks': [
+                    {'test_sql': "SELECT COUNT(*) FROM users", 'expected_result': 0},
+                    {'test_sql': "SELECT COUNT(*) FROM songs", 'expected_result': 0},
+                    {'test_sql': "SELECT COUNT(*) FROM artists", 'expected_result': 0},
+                    {'test_sql': "SELECT COUNT(*) FROM time", 'expected_result': 0},
+                    {'test_sql': "SELECT COUNT(*) FROM songplays", 'expected_result': 0}]
+}
+
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly',
+          catchup=False
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+# creat tables if not exist
+# creat_tables_task = PostgresOperator(
+#     task_id='creat_tables',
+#     dag=dag,
+#     sql='create_tables.sql',
+#     postgres_conn_id='redshift'
+# )
+
+# Note: it's existing log json path of log data
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    table='staging_events',
+    region="us-west-2",
+    s3_key="log_data",
+    s3_json_path = "s3://udacity-dend/log_json_path.json"
+)
+
+#  try run with small data set in song log
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    table='staging_songs',
+    region="us-west-2",
+    s3_key="song_data/A/A/A",
+    s3_json_path = "auto"
+)
+
+#  the fact table implicity allow append only
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag,
+    table = 'songplays',
+    sql_insert_table = SqlQueries.songplay_table_insert
+)
+
+# dim tables can be choose `truncate` or `append` when inserting data
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag,
+    table = 'users',
+    sql_insert_table = SqlQueries.user_table_insert,
+    insert_mode = 'truncate'
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag,
+    table = 'songs',
+    sql_insert_table = SqlQueries.song_table_insert,
+    insert_mode = 'truncate'
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag,
+    table = 'artists',
+    sql_insert_table = SqlQueries.artist_table_insert,
+    insert_mode = 'truncate'
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag,
+    table = 'time',
+    sql_insert_table = SqlQueries.time_table_insert,
+    insert_mode = 'truncate'
+)
+
+#  need provide table lists to be Check quality
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag,
+    tables = ["staging_events", "staging_songs", "songplays", "users", "songs", "artists", "time"],
+    dq_checks=default_args['dicts_checks']
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
+
+# Make task dependencies
+start_operator >> stage_events_to_redshift
+start_operator >> stage_songs_to_redshift
+
+stage_events_to_redshift >> load_songplays_table
+stage_songs_to_redshift >> load_songplays_table
+
+
+load_songplays_table >> load_song_dimension_table
+load_songplays_table >> load_user_dimension_table
+load_songplays_table >> load_artist_dimension_table
+load_songplays_table >> load_time_dimension_table
+
+
+load_song_dimension_table >> run_quality_checks
+load_user_dimension_table >> run_quality_checks
+load_artist_dimension_table >> run_quality_checks
+load_time_dimension_table >> run_quality_checks
+
+run_quality_checks >> end_operator
---
+++ Proj5_Data Pipeline/submission/home/airflow/plugins/operators/load_dimension.py
@@ -0,0 +1,41 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+# Dim table should be able to switching btw `truncate` or `append` : using `insert_mode`
+# trucate: clear data existing dim table prior to insert new data
+# append: just append new data to dim table
+class LoadDimensionOperator(BaseOperator):
+
+    ui_color = '#80BD9E'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id="redshift",
+                 table = "",
+                 sql_insert_table = "",
+                 insert_mode = "",
+                 *args, **kwargs):
+
+        super(LoadDimensionOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.table = table
+        self.insert_mode = insert_mode
+        self.sql_insert_table = sql_insert_table
+
+    def execute(self, context):
+        self.log.info("LoadDimensionOperator for dim_table {} is in progress >>>>>>>>>>>>> ...".format(self.table))
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        # do switching mode truncate or append
+        if (self.insert_mode == "append"):
+            redshift_hook.run(" INSERT INTO {} {} ".format(self.table, self.sql_insert_table) )
+
+        # truncate: means clear data inside table prior to insert new data, still keep table structure
+        if (self.insert_mode == "truncate"):
+            redshift_hook.run(" TRUNCATE TABLE  {}".format(self.table))
+            redshift_hook.run(" INSERT INTO {} {} ".format(self.table, self.sql_insert_table))
+
+        self.log.info("<<<<<<<<<<<<<<<<< .......................Finished to loading dim_table {}".format(self.table))
+        self.log.info("Move to Run Quality Check ... >>>")
